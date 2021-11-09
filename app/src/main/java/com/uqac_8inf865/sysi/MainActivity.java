0ca1670941216c49b06fd87a52e4ab553629ee58
Nathan <nathancuvier2929@gmail.com>
2021-11-09 00:23:14 -0500

    Implémentation de la NavigationBar et des différents fragments de le l'activité principale.

--- app/src/main/java/com/uqac_8inf865/sysi/MainActivity.java
+++ app/src/main/java/com/uqac_8inf865/sysi/MainActivity.java
@@ -1,8 +1,11 @@
 package com.uqac_8inf865.sysi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView,
+                new HomeFragment()).commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.page_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.page_map:
+                    selectedFragment = new MapsFragment();
+                    break;
+                case R.id.page_community:
+                    selectedFragment = new CommunityFragment();
+                    break;
+                case R.id.page_profil:
+                    selectedFragment = new ProfilFragment();
+                    break;
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .replace(R.id.fragmentContainerView,
+                        selectedFragment).commit();
+            }
+            return true;
+        });
+        bottomNavigationView.setOnItemReselectedListener(item -> {
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0){
+            getSupportFragmentManager().popBackStack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
